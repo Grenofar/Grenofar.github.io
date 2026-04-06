b096642d61f7d2e81eca98c2ecf9f433c3cfd701
Grenofar <rayanbelgacem747@gmail.com>
2026-04-06 19:24:26 +0200

    /

---
+++ presentation/script.js
@@ -0,0 +1,25 @@
+function AfficherFen2() {
+    document.getElementById('fen1').style.display = 'none';
+    document.getElementById('fen2').style.display = 'flex';
+}
+
+function RetourFen1() {
+    document.getElementById('fen2').style.display = 'none';
+    document.getElementById('fen1').style.display = 'flex';
+}
+function AfficherFen3() {
+    document.getElementById('fen1').style.display = 'none';
+    document.getElementById('fen3').style.display = 'flex';
+}
+function Retour2Fen1() {
+    document.getElementById('fen3').style.display = 'none';
+    document.getElementById('fen1').style.display = 'flex';
+}
+function AfficherFen4() {
+    document.getElementById('fen1').style.display = 'none';
+    document.getElementById('fen4').style.display = 'flex';
+}
+function Retour3Fen1() {
+    document.getElementById('fen4').style.display = 'none';
+    document.getElementById('fen1').style.display = 'flex';
+}
